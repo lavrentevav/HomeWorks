513d14f7fd4692838d96855e4b2d6839568c58a7
lavrentevav <lavrei@mail.ru>
2025-02-28 00:10:46 +0400

    Выполнены задачи 3_1-3_3

---
+++ Task 3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("a > b");
+            }
+            else
+            {
+                if (a == b)
+                {
+                    Console.WriteLine("a = b");
+                }
+                else
+                {
+                    Console.WriteLine("a < b");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
