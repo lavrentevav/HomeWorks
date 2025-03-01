037a7e508161f9044ce91cc31832c71501280879
lavrentevav <lavrei@mail.ru>
2025-03-01 23:53:48 +0400

    Выполнены все задачи по теме 3

---
+++ Task 3_10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_3_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество построенных домов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 100 >= 11 && n % 100 <= 14)
+            {
+                Console.WriteLine("Мы построили {0} домов", n);
+            }
+            else
+                switch (n % 10)
+                {
+                    case 1:
+                        Console.WriteLine("Мы построили {0} дом", n);
+                        break;
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("Мы построили {0} дома", n);
+                        break;
+                    default:
+                        Console.WriteLine("Мы построили {0} домов", n);
+                        break;
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
