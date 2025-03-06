fb37a24f9920b17b75c1426f530b906bc863f7e4
lavrentevav <lavrei@mail.ru>
2025-03-07 00:40:48 +0400

    Решены задачи 5_1-5_5

--- Task 5_1/Program.cs
+++ Task 5_1/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int n = 10;
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 101);
+                Console.Write("{0} ", array[i]);
+            }
+
+            int evenOddDifference = 0;
+            
+            foreach (var a in array)
+            {
+                if (a % 2 == 0)
+                {
+                    evenOddDifference++;
+                }
+                else
+                {
+                    evenOddDifference--;
+                }
+            }
+            Console.WriteLine();
+
+            if (evenOddDifference > 0)
+            {
+                Console.WriteLine("В массиве больше чётных чисел");
+            }
+            else if (evenOddDifference < 0)
+            {
+                Console.WriteLine("В массиве больше нечётных чисел");
+            }
+            else
+            {
+                Console.WriteLine("В массиве равное количество чётных и нечётных чисел");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task 5_3/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task_5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 51);
+                Console.Write("{0} ", array[i]);
+            }
+
+            int maxElement = array[0];
+            int minElement = array[0];
+            string maxElementIndexes = "1";
+            string minElementIndexes = "1";
+
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i] > maxElement)
+                {
+                    maxElement = array[i];
+                    maxElementIndexes = (i + 1).ToString();
+                }
+                else if (array[i] == maxElement)
+                {
+                    maxElementIndexes += ", " + (i + 1);
+                }
+                if (array[i] < minElement)
+                {
+                    minElement = array[i];
+                    minElementIndexes = (i + 1).ToString();
+                }
+                else if (array[i] == minElement)
+                {
+                    minElementIndexes += ", " + (i + 1);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Позиция в массиве максимального числа {0}: {1}", maxElement, maxElementIndexes);
+            Console.WriteLine("Позиция в массиве минимального числа {0}: {1}", minElement, minElementIndexes);
+            Console.ReadKey();
+        }
+    }
+}
