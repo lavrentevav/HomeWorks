88b79b9eedf688df40f0e1bb1c9a5ed839e70c88
lavrentevav <lavrei@mail.ru>
2025-03-15 19:03:52 +0400

    Выполнены все задачи по теме 6

---
+++ Task 6_3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите месяц отчёта:");
+            string month = Console.ReadLine();
+            Console.WriteLine("Введите год отчёта:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите общую сумму продаж:");
+            double money = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество проданных товаров:");
+            double quantity = Convert.ToDouble(Console.ReadLine());
+
+            double averageCost = money / quantity;
+
+            if (!string.IsNullOrEmpty(month))
+            {
+                month = char.ToUpper(month[0]) + month.Substring(1);
+            }
+
+            string moneyText = money.ToString("N");
+            string quantityText = quantity.ToString("N0");
+            string averageCostText = averageCost.ToString("N");
+
+            Console.WriteLine("--------------------------------\n" +
+                              "Отчёт о продажах за {0} {1}\n" +
+                              "--------------------------------\n" +
+                              "Общая сумма продаж: {2} р.\n" +
+                              "Количество проданных товаров: {3} шт.\n" +
+                              "Средняя стоимость товаров: {4} р.\n" +
+                              "--------------------------------\n",
+                              month, year, moneyText, quantityText, averageCostText);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 6_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Task_6_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите возраст:");
+            string age = Console.ReadLine();
+            Console.WriteLine("Введите город:");
+            string place = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Имя: ");
+            sb.Append(char.ToUpper(name[0]) + name.Substring(1));
+            sb.Append(", Возраст: ");
+            sb.Append(age);
+            sb.Append(", Город: ");
+            sb.Append(char.ToUpper(place[0]) + place.Substring(1));
+
+            Console.WriteLine(sb);
+            Console.ReadKey();
+        }
+    }
+}
