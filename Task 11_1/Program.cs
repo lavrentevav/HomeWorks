d5c8f2bea2665eb8c1751e9e71302deed32a2216
lavrentevav <lavrei@mail.ru>
2025-05-25 16:21:55 +0400

    Выполнено домашнее задание по теме 11

---
+++ Task 11_1/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Task_11_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Массив животных
+            Animal[] animals = new Animal[]
+            {
+                new Dog(),
+                new Cat(),
+            };
+
+            // Демонстрация полиморфизма
+            foreach (var animal in animals)
+                animal.ShowInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Animal
+    {
+        // Абстрактное свойство для имени
+        public abstract string Name { get; }
+
+        // Абстрактный метод
+        public abstract string Say();
+
+        // Обычный (не абстрактный) метод
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} говорит: \"{Say()}\"");
+            Console.WriteLine();
+        }
+    }
+    // Класс "Собака"
+    public class Dog : Animal
+    {
+        public override string Name => "Собака";
+
+        public override string Say()
+        {
+            return "Гав!";
+        }
+    }
+
+    // Класс "Кошка"
+    public class Cat : Animal
+    {
+        public override string Name => "Кошка";
+
+        public override string Say()
+        {
+            return "Мяу!";
+        }
+    }
+}
