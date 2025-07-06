a35c7611e947ef344568368d4ed0382d773c5dd6
lavrentevav <lavrei@mail.ru>
2025-07-07 01:35:14 +0400

    Выполнено домашнее задание по теме WPF №1

---
+++ WpfApp_Task 1_1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.PerformanceData;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Task_1_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _clickCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickButton_Click(object sender, RoutedEventArgs e)
+        {
+            _clickCount++;
+            CounterText.Text = _clickCount.ToString();
+        }
+    }
+}
---
+++ WpfApp_Task 1_2_TrafficLight/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_Task_1_2_TrafficLight
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
