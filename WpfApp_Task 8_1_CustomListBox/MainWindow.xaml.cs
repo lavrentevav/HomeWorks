182cbb5ce0eebc21e2b116fae7126d33c4697071
lavrentevav <lavrei@mail.ru>
2025-09-06 18:12:47 +0400

    Выполнено домашнее задание по теме WPF 8

---
+++ WpfApp_Task 8_1_CustomListBox/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Task_8_1_CustomListBox
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products =
+            [
+                new Product("Холодильник", 45000.00m, "images/fridge.jpg", ProductCategory.HouseholdEquipment),
+                new Product("Яблоки", 140.00m, "images/apple.jpg", ProductCategory.Food),
+                new Product("Бананы", 160.00m, "images/banana.jpg", ProductCategory.Food)
+            ];
+            DataContext = this;
+        }        
+    }
+}
---
+++ WpfApp_Task 8_1_CustomListBox/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Task_8_1_CustomListBox
+{
+    public enum ProductCategory
+    {
+        Food,
+        HouseholdEquipment
+    }
+
+    public class Product(string name, decimal price, string imagePath, ProductCategory category)
+    {
+        public string ProductName { get; set; } = name;
+        public decimal Price { get; set; } = price;
+        public string ImagePath { get; set; } = imagePath;
+        public ProductCategory Category { get; set; } = category;
+    }
+}
