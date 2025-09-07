2fea73f69fae577052afa90155226fd7741cb8a0
lavrentevav <lavrei@mail.ru>
2025-09-07 18:33:10 +0400

    Выполнено домашнее задание по теме WPF 9

---
+++ WpfApp_Task 9_1_ChangeBackground/CustomCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp_Task_9_1_ChangeBackground
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand ChangeColor = new(
+            "Изменить цвет",
+            "ChangeColor",
+            typeof(CustomCommands),
+            [
+                new KeyGesture(Key.C, ModifierKeys.Control)
+            ]);
+    }
+}
